0fd187ef627f4a65f2fd780234c901fc6043424e
asrulcahyadiputra <asrulcahyadiputra99@gmail.com>
2022-02-03 13:56:41 +0800

    handle endpoint with parameter

--- main.go
+++ main.go
@@ -11,6 +11,8 @@ func main() {
 
 	router.GET("/", rootHandler)
 
+	router.GET("/customer/:id", customerHandler)
+
 	router.Run(":8000")
 }
 
@@ -18,5 +20,14 @@ func rootHandler(c *gin.Context){
 	c.JSON(http.StatusOK, gin.H{
 		"status" : true,
 		"messega" : "Welcome to web service Golang with gin",
+	})
+}
+
+func customerHandler(c *gin.Context){
+	id := c.Param("id")
+
+	c.JSON(http.StatusOK, gin.H{
+		"customer_id" : id,
+		"customer_name" : "Asrul Cahyadi Putra",
 	})
 }
