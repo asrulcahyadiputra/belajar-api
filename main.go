58dd6668f4199777fdccba6cc014cf1509918f7a
asrulcahyadiputra <asrulcahyadiputra99@gmail.com>
2022-02-03 13:47:49 +0800

    change  anonymous function to function

--- main.go
+++ main.go
@@ -9,12 +9,14 @@ import (
 func main() {
 	router := gin.Default()
 
-	router.GET("/", func (c *gin.Context )  {
-		c.JSON(http.StatusOK, gin.H{
-			"status" : true,
-			"messega" : "Welcome to web service Golang with gin",
-		})
-	})
+	router.GET("/", rootHandler)
 
 	router.Run(":8000")
+}
+
+func rootHandler(c *gin.Context){
+	c.JSON(http.StatusOK, gin.H{
+		"status" : true,
+		"messega" : "Welcome to web service Golang with gin",
+	})
 }
