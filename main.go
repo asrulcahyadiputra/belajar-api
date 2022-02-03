073d4e2a92e8c070071dfcf1c36ecc4b51c3b769
asrulcahyadiputra <asrulcahyadiputra99@gmail.com>
2022-02-03 15:38:18 +0800

    validation post request

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 )
 
 func main() {
@@ -14,6 +17,9 @@ func main() {
 	router.GET("/customer/:id", customerHandler)
 
 	router.GET("/query", queryHandler)
+
+	router.POST("/customer", postCustomerHandler)
+
 	router.Run(":8000")
 }
 
@@ -41,5 +47,37 @@ func queryHandler(c *gin.Context){
 		"message" : "record found!",
 		"customer_id" : "CUS-01",
 		"customer_name" : name,
+	})
+}
+
+type InputCustomer struct {
+	Name string 		`json:"name" binding:"required"`
+	Age json.Number		`json:"age" binding:"required,number"`
+	ParentName string 	`json:"parent_name" binding:"required"`
+}
+
+func postCustomerHandler(c *gin.Context){
+	var inputCustomer InputCustomer
+
+	err := c.ShouldBindJSON(&inputCustomer)
+
+	if err != nil {
+		errorMessages := []string{} //slice
+		for _, e := range err.(validator.ValidationErrors){
+			errorMessage := fmt.Sprintf("error on field %s, rules: %s", e.Field(), e.ActualTag())
+			errorMessages = append(errorMessages, errorMessage) //append to slice errorMessages
+		}
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error" : errorMessages,
+		})		
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"status" : true,
+		"message" : "Post request successfully!",
+		"name" : inputCustomer.Name,
+		"parent_name" : inputCustomer.ParentName,
+		"age" : inputCustomer.Age,
 	})
 }
